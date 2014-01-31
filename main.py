860590451f737ffcabeae2701e794d1b552e41a8
Walter Michelin <wmichelin@gmail.com>
2014-01-31 13:39:22 -0500

    checks for mentions of “seattle” and “denver” in the top comments of reddit.com/r/nfl and prints the results

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ user_agent = ("comment scraper by /u/wmichelin"
 r = praw.Reddit(user_agent=user_agent)
 
 all_comments = r.get_comments('nfl')
-
-print 'before for each loop'
+seattleCount = 0
+denverCount = 0
 
 for comment in all_comments:
 	tempString = comment.body
@@ -15,7 +15,26 @@ for comment in all_comments:
 	words = tempString.split()
 	
 	for word in words:
+		print word
+		tempString = ''
 		word = word.lower()
-		if 'the' in word:
-			print 'found!'
+		if 'seattle' in word:
+			print 'SEATTLE'
+			seattleCount = seattleCount + 1
+		if 'denver' in word:
+			print 'DENVER'
+			denverCount = denverCount + 1 
+
+
+tempString = 'Times seattle mentioned: '
+tempString += str(seattleCount)
+print tempString
+tempString = ''
+
+tempString = 'Times denver mentioned: '
+tempString += str(denverCount)
+print tempString
+
+
+
 		
