e91000ac5835549c766161a662bc423b77faa7d4
Walter Michelin <wmichelin@gmail.com>
2014-02-05 15:39:52 -0500

    added twitter functionality
    still need to implement twitter status posts and change super bowl theme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import praw
 import time
+import tweepy
+import urllib
 
 seattleCount = 0
 denverCount = 0
@@ -8,8 +10,22 @@ user_agent = ("NFL superbowl mention counter by walter michelin"
 			  "github.com/wmichelin")
 
 r = praw.Reddit(user_agent=user_agent)
-
 rNFL = r.get_subreddit('nfl')
+
+consumer_key = 'Ud6dfcMuiQGV2uh6kpkDng'
+consumer_secret = 'OsArxd12XeLXrCzCqJKf1I9fhIZNWAGFeq7D7o2uM'
+
+access_token = '2329099704-SHifsHTY91SH1UckrlcK3lpWOnHyjiROwZC4hqA'
+access_secret = 'nWomFXS2kzblfbv1VHQ06mB4CHB9ht6LrzxCLPTaMQvwu'
+
+auth = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tweepy.API(auth)
+
+api.update_status('hello world')
+
+
+
 
 while True:	
 	
@@ -25,10 +41,10 @@ while True:
 	
 			for word in words:
 				word = word.lower()
-				if 'seattle' in word:
+				if 'seattle' in word or 'seahawks' in word:
 					print 'SEATTLE'
 					seattleCount = seattleCount + 1
-				if 'denver' in word:
+				if 'denver' in word or 'broncos' in word:
 					print 'DENVER'
 					denverCount = denverCount + 1 
 
@@ -44,7 +60,7 @@ while True:
 	tempString += str(denverCount)
 	print tempString
 	
-	time.sleep(300)
+	time.sleep(10)
 
 
 
