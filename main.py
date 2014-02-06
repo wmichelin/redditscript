a27065e9ac06d47094d98e4a382e92d700b4856b
Walter Michelin <wmichelin@gmail.com>
2014-02-06 18:07:56 -0500

    bug fixes

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import praw
 import time
+from datetime import datetime
 import tweepy
 import string 
 import sys
 
 class countObj:
-	word = ''
+	global word
 	global count
+	word = ''
 	count = 0
 	
-	def __init__(self, word):
+	def __init__(self, word, count):
 		self.word = word
-		self.count = 0
+		self.count = count
 
 	def __str__(self):
 		temp = ''
@@ -20,24 +22,27 @@ class countObj:
 		temp += str(self.count)
 		return temp
 		
+	def setcount(num):
+		self.count = num
+		
 	def __lt__(self, other):
 		return (self.count > other.count)
 		
-	def get(self):
-		return self
+	def copy(self):
+		temp = countObj(self.word, (self.count+1))
+		return temp
+		
+	def getCount(self):
+		return count
 			
-	def increment(self):
-		global count
-		count += 1
-		#print '*******INCREMENTING********'
-		#print count
 		
 alreadyDone = []
 user_agent = ("reddit trending words by walter michelin"
 			  "github.com/wmichelin")
 
 r = praw.Reddit(user_agent=user_agent)
-all = r.get_subreddit('all')
+sub = 'gaming'
+all = r.get_subreddit(sub)
 
 consumer_key = 'Ud6dfcMuiQGV2uh6kpkDng'
 consumer_secret = 'OsArxd12XeLXrCzCqJKf1I9fhIZNWAGFeq7D7o2uM'
@@ -49,6 +54,9 @@ auth = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
+def tweet(self, input):
+	api.update_status(input)
+
 #api.update_status('hello world')
 
 common = 'the be to of and a in that have i it for not on with he as you do at word this but his by'
@@ -59,6 +67,7 @@ common = common + ' now look only come its over think also back after use two ho
 common = common + ' well way even new want because any these give day most us'
 common = common + ' oh thats is was are im thru has too lol here someone were very id cant'
 common = common + ' 1 2 3 4 5 6 7 8 9 0 . .. ... .... .....'
+common = common + ' become dont'
 
 common_words = []
 common_words = common.split()
@@ -66,76 +75,90 @@ common_words = common.split()
 obj = []
 duplicate = []
 
+timesrun = 0
+timestarted = str(datetime.now())
+
 while True:	
 	for x in range (0 , 4):
 		sys.stdout.write('\r....')
 		sys.stdout.write('\r' + 'loading' + '.' * x)
 		sys.stdout.flush()
-		time.sleep(.2)
 		
-		
-	tempString = ''
 	checker = False 
+	dupechecker = False
 	all_comments = r.get_comments(all, limit=None)
-
+	anynew = False
+	
 	for comment in all_comments:
 		if comment.body not in alreadyDone:
+			anynew = True
 			alreadyDone.append(comment.body)
 	 		tempString = comment.body
 			words = []
 			words = tempString.split()
-	
+			
+
+			#checking words from comments against previous words
+			i = 0
 			for word in words:
 				for c in string.punctuation:
 					word = word.replace(c,"")
-				word = ''.join(word.split())
-				if word is not '':
-					if len(word) > 1:
-						if len(word) < 18:
-							temp = countObj('')
-							checker = False
-							word = word.lower()
-							print word
+				word = word.lower()
+				word = word.strip()
+				if word not in common_words:	
+					print word
+					checker = False
+					for x in obj:
+						if x.word is word:
+							checker = True
+					if checker:
+						j = 0
+						dupechecker = False
+						for thing in duplicate:
+							if thing.word is word:
+								dupechecker = True
+								tempcount = thing.count
+								temp = duplicate[j].copy()
+								temp.setcount(tempcount + 1)
+								duplicate[j] = temp
+							j = j+1
+						if not dupechecker:
+							temp = x.copy()
+							duplicate.append(temp)
+					else:
+						temp = countObj(word, 0)
+						obj.append(temp)
+					
+					
+		
 
-							if word not in common_words:
-								for thing in obj:
-									if word is thing.word:
-										print word
-										checker = True
-										print '*************DUPLICATE!!!!!!**********' + word
-										thing.get().increment()
-									
-								if checker == False:
-										temp = countObj(word) 
-										obj.append(temp)
-										tempstr = temp.__str__()
-										print 'appending! ' + tempstr
-								
-		#for thing in obj:
-	#print 'PRINT OBJECT ARRAY HERE'
-	for thing in obj:
-		if thing.count > 0:
-			if thing not in duplicate:
-				duplicate.append(thing)
-				print thing.word
-				print thing.count
+	print 'size of object array: ' + str(len(obj))
+	print 'size of DUPLICATE array: ' + str(len(duplicate))
+	print '****************SORTING AND PRINTING DUPLICATES******************'
+	duplicate.sort()
+	for thing in duplicate:
+		print thing
+		
+	output = 'Top 5 trending words on /r/' + sub + ' since ' + timestarted
+	output += '\n'
+	for x in range (0, 5):
+		output += duplicate[x].word + ' has been mentioned ' + str(duplicate[x].count + 1) + ' times.'
+		output += '\n'
+	print str(timesrun)
+	print output
+	if anynew:
+		timesrun =  timesrun + 1
+		
+	if (timesrun%10 == 0):
+		tweet(output)
+		print '*********** NEW TWEET ****************'
+		
 	
-	if len(duplicate) > 0:	
-		duplicate.sort()
-	
-	
-	print '*****************PRINTING DUPLICATE!!!!!!************************'
-	print str(len(duplicate))
-	for item in duplicate:
-		print item.word
-		print item.count
+	time.sleep(10)
 	
 	
 	
-	
-	print 'sleeping'
-	time.sleep(15)
-	
+		
 	
 
 
